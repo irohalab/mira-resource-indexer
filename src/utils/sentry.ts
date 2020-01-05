6aee75d15d3f5472d7ffdc2d8460abe272d66e9b
everett_summer <everett_summer@iroha.io>
2020-01-05 10:44:25 +0800

    add release to sentry

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,8 +1,13 @@
 import {init, captureMessage as sentryCaptureMessage, captureException as sentryCaptureException} from '@sentry/node';
 
 const DSN = process.env.SENTRY_DSN;
+const RELEASE = process.env.RELEASE || process.env.npm_package_version;
+
 if (DSN) {
-    init({dsn: DSN});
+    init({
+        dsn: DSN,
+        release: RELEASE
+    });
 }
 
 export function captureException(err: any) {
