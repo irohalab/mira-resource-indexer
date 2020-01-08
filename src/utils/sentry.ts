fda8bf039232471b9149f64782477f6955d257b8
acgotaku <acgotaku311@gmail.com>
2020-01-08 23:23:48 +0900

    fix tsconfig root

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,5 +1,6 @@
 import { init, captureMessage as sentryCaptureMessage, captureException as sentryCaptureException } from '@sentry/node';
-import { version } from '../../package.json';
+// tslint:disable-next-line
+const { version } = require('../package.json');
 const DSN = process.env.SENTRY_DSN;
 if (DSN) {
     init({ dsn: DSN, release: `indexer@v${version}` });
